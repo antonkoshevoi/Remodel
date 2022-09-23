7e6deeba06f046694afeebcfc95f399497329a57
antonkoshevoi <anton.koshevoi.dev@gmail.com>
2022-09-23 17:17:44 +0300

    feat: add validation for form steps

--- js/pages/find-local-pros/formStepper.js
+++ js/pages/find-local-pros/formStepper.js
@@ -4,8 +4,15 @@ showTab(currentStep);
 
 function showTab(step) {
   let currentForm = document.getElementsByClassName("form__step");
+  currentForm[step].classList.add('form__active');
+}
 
-  if(currentStep === 2){
+
+function nextPrev(step) {
+  let currentForm = document.getElementsByClassName("form__step");
+  if (step == 1 && !validateForm()) return false;
+  
+  if(currentStep === 1){
     let bath = document.getElementById('bathroom');
     let window = document.getElementById('window');
     let both = document.getElementById('both');
@@ -17,35 +24,38 @@ function showTab(step) {
       let bathForm = document.getElementById('form-step-options');
       bathForm.innerHTML = `
         <div class="step__title">
-          <h2 class="">What type of bath issues do you have</h2>
+          <h2>What type of bath issues do you have</h2>
         </div>
         <div class="step__body">
           <div class="step__item">
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="option" id="option1">
-                <label class="form-check-label" for="Option1">
+                <input class="form-check-input" type="radio" name="option" id="option1" value="option1">
+                <label class="form-check-label" for="option1">
                   Option1
                 </label>
               </div>
             </div>
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="option" id="option2">
-                <label class="form-check-label" for="Option2">
+                <input class="form-check-input" type="radio" name="option" id="option2" value="option2">
+                <label class="form-check-label" for="option2">
                   Option2
                 </label>
               </div>
             </div>
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="option" id="option3">
-                <label class="form-check-label" for="Option3">
+                <input class="form-check-input" type="radio" name="option" id="option3" value="option3">
+                <label class="form-check-label" for="option3">
                   Option3
                 </label>
               </div>
             </div>
+            <i class="step__error">Please select an option (radio button questions)</i>
           </div>
+
+          
           <div class="step__item mt-5 btn__both d-flex justify-content-between">
             <div class="btn__prev">
               <button class="btn__step btn__outline" onclick="nextPrev(-1)" id="btnPrev"> </button>
@@ -72,28 +82,29 @@ function showTab(step) {
           <div class="step__item">
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="wind" id="firstSelect">
-                <label class="form-check-label" for="firstSelect">
+                <input class="form-check-input" type="radio" name="amount" id="selectOne" >
+                <label class="form-check-label" for="selectOne">
                   1-3
                 </label>
               </div>
             </div>
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="wind" id="secondSelect">
-                <label class="form-check-label" for="secondSelect">
+                <input class="form-check-input" type="radio" name="amount" id="selectTwo" >
+                <label class="form-check-label" for="selectTwo">
                   3-10
                 </label>
               </div>
             </div>
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="wind" id="thirdSelect">
-                <label class="form-check-label" for="thirdSelect">
+                <input class="form-check-input" type="radio" name="amount" id="selectThree">
+                <label class="form-check-label" for="selectThree">
                   10+
                 </label>
               </div>
             </div>
+            <i class="step__error">Please select an option (radio button questions)</i>
           </div>
           
           <div class="step__item mt-5 btn__both d-flex justify-content-between">
@@ -114,48 +125,49 @@ function showTab(step) {
       bothForm.className = 'd-flex flex-column justify-content-center align-items-center form__step';
       bothForm.id = 'form-step-window';
       bothForm.innerHTML = `
-        <div class="step__title">
-          <h2 class="">Choose an amount below</h2>
+      <div class="step__title">
+        <h2 class="">Choose an amount below</h2>
+      </div>
+      <div class="step__body">
+        <div class="step__item">
+          <div class="mt-3">
+            <div class="form-check d-flex align-items-center justify-content-center step__check">
+              <input class="form-check-input" type="radio" name="amount" id="selectOne" >
+              <label class="form-check-label" for="selectOne">
+                1-3
+              </label>
+            </div>
+          </div>
+          <div class="mt-3">
+            <div class="form-check d-flex align-items-center justify-content-center step__check">
+              <input class="form-check-input" type="radio" name="amount" id="selectTwo" >
+              <label class="form-check-label" for="selectTwo">
+                3-10
+              </label>
+            </div>
+          </div>
+          <div class="mt-3">
+            <div class="form-check d-flex align-items-center justify-content-center step__check">
+              <input class="form-check-input" type="radio" name="amount" id="selectThree">
+              <label class="form-check-label" for="selectThree">
+                10+
+              </label>
+            </div>
+          </div>
+          <i class="step__error">Please select an option (radio button questions)</i>
         </div>
-        <div class="step__body">
-          <div class="step__item">
-            <div class="mt-3">
-              <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="wind" id="firstSelect">
-                <label class="form-check-label" for="firstSelect">
-                  1-3
-                </label>
-              </div>
-            </div>
-            <div class="mt-3">
-              <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="wind" id="secondSelect">
-                <label class="form-check-label" for="secondSelect">
-                  3-10
-                </label>
-              </div>
-            </div>
-            <div class="mt-3">
-              <div class="form-check d-flex align-items-center justify-content-center step__check">
-                <input class="form-check-input" type="radio" name="wind" id="thirdSelect">
-                <label class="form-check-label" for="thirdSelect">
-                  10+
-                </label>
-              </div>
-            </div>
+        
+        <div class="step__item mt-5 btn__both d-flex justify-content-between">
+          <div class="btn__prev">
+            <button class="btn__step btn__outline" onclick="nextPrev(-1)" id="btnPrev"> </button>
+          </div>
+      
+          <div class="btn__next">
+            <button class="btn__step btn__primary btn__image__primary" onclick="nextPrev(1)" id="btnNext">Next</button>
           </div>
           
-          <div class="step__item mt-5 btn__both d-flex justify-content-between">
-            <div class="btn__prev">
-              <button class="btn__step btn__outline" onclick="nextPrev(-1)" id="btnPrev"> </button>
-            </div>
-        
-            <div class="btn__next">
-              <button class="btn__step btn__primary btn__image__primary" onclick="nextPrev(1)" id="btnNext">Next</button>
-            </div>
-            
-          </div>
         </div>
+      </div>
       `;
       let optionsForm = document.getElementById('form-step-options');
       optionsForm.innerHTML =`
@@ -167,7 +179,7 @@ function showTab(step) {
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
                 <input class="form-check-input" type="radio" name="option" id="option1">
-                <label class="form-check-label" for="Option1">
+                <label class="form-check-label" for="option1">
                   Option1
                 </label>
               </div>
@@ -175,7 +187,7 @@ function showTab(step) {
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
                 <input class="form-check-input" type="radio" name="option" id="option2">
-                <label class="form-check-label" for="Option2">
+                <label class="form-check-label" for="option2">
                   Option2
                 </label>
               </div>
@@ -183,11 +195,12 @@ function showTab(step) {
             <div class="mt-3">
               <div class="form-check d-flex align-items-center justify-content-center step__check">
                 <input class="form-check-input" type="radio" name="option" id="option3">
-                <label class="form-check-label" for="Option3">
+                <label class="form-check-label" for="option3">
                   Option3
                 </label>
               </div>
             </div>
+            <i class="step__error">Please select an option (radio button questions)</i>
           </div>
           <div class="step__item mt-5 btn__both d-flex justify-content-between">
             <div class="btn__prev">
@@ -201,50 +214,83 @@ function showTab(step) {
           </div>
         </div>
       `;
-      optionsForm.after(bothForm);
+      if(!document.getElementById('form-step-window')){
+        optionsForm.after(bothForm);
+      }
+      //optionsForm.after(bothForm);
     }
   }
-  currentForm[step].classList.add('form__active');
-}
-
-function nextPrev(step) {
-  let currentForm = document.getElementsByClassName("form__step");
-  //if (step == 1 && !validateForm()) return false;
   currentForm[currentStep].classList.remove('form__active');
   currentStep = currentStep + step;
   showTab(currentStep);
 }
 
 function validateForm() {
-  let valid = true;
-  let inputReg = /^[0-9]{5}(?:-[0-9]{4})?$/;
+  let isValid = true;
   let currentForm = document.querySelectorAll(".form__step")[currentStep];
   let inputLength = currentForm.querySelectorAll("input").length;
   for (i = 0; i < inputLength; ++i) {
     let inputs = currentForm.querySelectorAll('input')[i];
     if(inputs.getAttribute("type") === 'text'){
-      if(inputs.value === '' || !inputReg.test(inputs.value)){
+      let zipReg = /^[0-9]{5}(?:-[0-9]{4})?$/;
+      if(inputs.value === '' || !zipReg.test(inputs.value)){
         currentForm.querySelector('.step__error').classList.add('active__error');
-        valid = false;
+        isValid = false;
       }else{
         currentForm.querySelector('.step__error').classList.remove('active__error');
       }
     }
     if(inputs.getAttribute("type") === 'radio'){
       if(!inputs.checked){
-        console.log('no')
         currentForm.querySelector('.step__error').classList.add('active__error');
-        valid = false;
+        isValid = false;
       }else{
         currentForm.querySelector('.step__error').classList.remove('active__error');
-        valid = true;
+        isValid = true;
         break;
       }
     }
-    
-  }
-  return valid; 
+    if(inputs.id === 'first-name' || inputs.id === 'last-name'){
+      let nameInputs = currentForm.querySelectorAll('input');
+      if(nameInputs[0].value === '' || nameInputs[0].value.length <= 2){
+        currentForm.querySelector('.error__name').classList.add('active__error');
+        isValid = false;
+      }else{
+        currentForm.querySelector('.error__name').classList.remove('active__error');
+      }
+      if(nameInputs[1].value === '' || nameInputs[1].value.length <= 2){
+        currentForm.querySelector('.error__sename').classList.add('active__error');
+        isValid = false;
+      }else{
+        currentForm.querySelector('.error__sename').classList.remove('active__error');
+      }
+      if(nameInputs[i].value.length > 2){
+        isValid = true;
+      }
+    }
+    if(inputs.id === 'email-adress' || inputs.id === 'number-phone'){
+      let infoInputs = currentForm.querySelectorAll('input'),
+      emailReg = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/,
+      numberReg = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/;
+      if(infoInputs[0].value === '' || !emailReg.test(infoInputs[0].value)){
+        currentForm.querySelector('.error__email').classList.add('active__error');
+        isValid = false;
+      }else{
+        currentForm.querySelector('.error__email').classList.remove('active__error');
+      }
+      if(infoInputs[1].value === '' || !numberReg.test(infoInputs[1].value)){
+        currentForm.querySelector('.error__phone').classList.add('active__error');
+        isValid = false;
+      }else{
+        currentForm.querySelector('.error__phone').classList.remove('active__error');
+      }
+      if(emailReg.test(infoInputs[0].value) && numberReg.test(infoInputs[1].value) ){
+        isValid = true;
+      }
 
+    }
+  }
+  return isValid; 
 }
 
 
