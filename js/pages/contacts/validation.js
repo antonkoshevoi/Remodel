092cf29a759d28bbce9c3245e4641d96535a8100
antonkoshevoi <anton.koshevoi.dev@gmail.com>
2022-09-28 16:46:42 +0300

    feat: create layout contacts page

---
+++ js/pages/contacts/validation.js
@@ -0,0 +1,67 @@
+let form = document.getElementById('contact-us-form'),
+    formInputs = document.querySelectorAll('.contact__input'),
+    inputName = document.querySelector('.contact__input__name')
+    inputEmail = document.querySelector('.contact__input__email'),
+    inputPhone = document.querySelector('.contact__input__phone'),
+    inputArea = document.querySelector('.contact__area'),
+    error = document.querySelectorAll('.step__error');
+
+function validateEmail (email)  {
+  let emailReg = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/;
+  return emailReg.test(email);
+}
+
+function validatePhone (phone)  {
+  let phoneReg = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return phoneReg.test(phone);
+}
+
+form.onsubmit = function () {
+  let emailValue = inputEmail.value,
+      phoneValue = inputPhone.value,
+      areaValue = inputArea.value,
+      emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+    
+  formInputs.forEach((input) => {
+    if(input.value === ''){
+      input.closest('.input-container')
+      .closest('.contact__form')
+      .querySelector('.step__error')
+      .classList.add('active__error');
+    }else{
+      input.closest('.input-container')
+      .closest('.contact__form')
+      .querySelector('.step__error')
+      .classList.remove('active__error');
+    }
+  })
+
+  if(emptyInputs.length === ''){
+    return false;
+  }
+
+  if(!validateEmail(emailValue)){
+    document.querySelector('.error__email').classList.add('active__error');
+    return false;
+  } else{
+    document.querySelector('.error__email').classList.remove('active__error');
+  }
+
+  if(!validatePhone(phoneValue)){
+    document.querySelector('.error__phone').classList.add('active__error');
+    return false;
+  } else{
+    document.querySelector('.error__phone').classList.remove('active__error');
+  }
+  
+  if(areaValue === '' ){
+    document.querySelector('.error__area').classList.add('active__error');
+    return false;
+  } else{
+    document.querySelector('.error__area').classList.remove('active__error');
+  }
+
+}
+
+
+
--- js/pages/find-local-pros/inputLabel.js
+++ js/pages/find-local-pros/inputLabel.js
@@ -18,20 +18,32 @@ const FloatLabel = (() => {
   
   // register events
   const bindEvents = (element) => {
-    const floatField = element.querySelector('input');
-    floatField.addEventListener('focus', handleFocus);
-    floatField.addEventListener('blur', handleBlur);    
+    if(element.querySelector('input')){
+      const floatField = element.querySelector('input');
+      floatField.addEventListener('focus', handleFocus);
+      floatField.addEventListener('blur', handleBlur);
+    }
+    if(element.querySelector('textarea')){
+      const floatArea = element.querySelector('textarea');
+      floatArea.addEventListener('focus', handleFocus);
+      floatArea.addEventListener('blur', handleBlur); 
+    }  
   };
   
   // get DOM elements
   const init = () => {
     const floatContainers = document.querySelectorAll('.input-container');
-    
     floatContainers.forEach((element) => {
-      if (element.querySelector('input').value) {
+      if (element.querySelector('input')) {
+        if (element.querySelector('input').value) {
           element.classList.add('active');
-      }      
-      
+        } 
+      }    
+      if (element.querySelector('textarea')) {
+        if (element.querySelector('textarea').value) {
+          element.classList.add('active');
+        } 
+      }  
       bindEvents(element);
     });
   };
